0a9076a3be81083d5946aa0e27d751cc2d065ae9
user <kilavyk@yandex.ru>
2025-06-11 20:27:05 +0300

    Connect the PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ORM in Django",
+        "USER": "postgres",
+        "PASSWORD": "192202",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
