bded580559bfd24a83c69b2acc45b264e8a0bbb2
user <kilavyk@yandex.ru>
2025-07-08 21:45:10 +0300

    the logic of registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth import logout
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            # Отправка приветственного письма
+            send_mail(
+                'Добро пожаловать!',
+                'Спасибо за регистрацию в нашем сервисе.',
+                settings.EMAIL_HOST_USER,
+                [user.email],
+                fail_silently=False,
+            )
+
+            messages.success(request, 'Вы успешно зарегистрированы!')
+            return redirect('users:login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('catalog:home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('catalog:home')
