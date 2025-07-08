426bdd15cc6d5817a0db233aade8430db8271520
user <kilavyk@yandex.ru>
2025-07-08 21:49:08 +0300

    registration form templates

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('catalog.urls', namespace="catalog")),
     path('blog/', include('blog.urls')),  # добавляем URL блога
+    path('users/', include('users.urls', namespace='users')),  # добавляем URL авторизации
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
