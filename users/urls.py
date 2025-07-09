611e88f2269ea7e5121936c80bfd0c4dccb0cf27
user <kilavyk@yandex.ru>
2025-07-09 18:17:16 +0300

    updated urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
+
+from .views import DeleteAccountView, ProfileView, UserLoginView, UserLogoutView, UserRegisterView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('delete-account/', DeleteAccountView.as_view(), name='delete_account'),
 ]
