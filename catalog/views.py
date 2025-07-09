0f9e93404ca1f66657519f248433a63fe00fbc16
user <kilavyk@yandex.ru>
2025-07-09 18:12:47 +0300

    closing management for anonymous users

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,14 @@
-from django.views.generic import (
-    ListView, DetailView, CreateView, UpdateView, DeleteView, View
-)
-from django.urls import reverse_lazy
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView, View
 
-from .models import Product
 from .forms import ProductForm
+from .models import Product
 
 
 class HomeView(ListView):
@@ -41,32 +41,36 @@ class ContactsView(View):
         return HttpResponse(response_text, content_type='text/plain; charset=utf-8')
 
 
-class ProductCreateView(SuccessMessageMixin, CreateView):
+class ProductCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:home')
     success_message = "Продукт успешно создан!"
     template_name = 'catalog/product_form.html'
+    login_url = reverse_lazy('users:login')  # URL для перенаправления неавторизованных
 
     def form_invalid(self, form):
         messages.error(self.request, 'Пожалуйста, исправьте ошибки в форме')
         return super().form_invalid(form)
 
-class ProductUpdateView(SuccessMessageMixin, UpdateView):
+
+class ProductUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:home')
     success_message = "Продукт успешно обновлён!"
     template_name = 'catalog/product_form.html'
+    login_url = reverse_lazy('users:login')
 
     def form_invalid(self, form):
         messages.error(self.request, 'Пожалуйста, исправьте ошибки в форме')
         return super().form_invalid(form)
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:home')
     template_name = 'catalog/product_confirm_delete.html'
+    login_url = reverse_lazy('users:login')
 
     def delete(self, request, *args, **kwargs):
         messages.success(request, "Продукт успешно удалён!")
