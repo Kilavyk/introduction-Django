5c7ef97bf9d1d146c4477ffbb709e5a7d52d15b2
user <kilavyk@yandex.ru>
2025-07-11 19:54:33 +0300

    big fix admin panel

--- users/models.py
+++ users/models.py
@@ -1,6 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Необходимо указать адрес электронной почты')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
@@ -11,6 +32,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'  # Используем email для авторизации
     REQUIRED_FIELDS = []  # Убираем username из обязательных полей
+
+    objects = UserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
