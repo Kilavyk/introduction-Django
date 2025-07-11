5c6c64a76b565e228a1b3d26d667ed23fb8c4fe3
user <kilavyk@yandex.ru>
2025-07-11 20:04:56 +0300

    Add status to Product model

--- catalog/models.py
+++ catalog/models.py
@@ -69,10 +69,27 @@ class Product(models.Model):
         verbose_name="Дата последнего изменения"
     )
 
+    # Добавляем статус публикации
+    PUBLISH_STATUS = [
+        ('draft', 'Черновик'),
+        ('published', 'Опубликован'),
+        ('rejected', 'Отклонен'),
+    ]
+
+    status = models.CharField(
+        max_length=10,
+        choices=PUBLISH_STATUS,
+        default='draft',
+        verbose_name="Статус публикации"
+    )
+
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["name", "category", "purchase_price"]
-
+        permissions = [
+            ("can_unpublish_product", "Может отменять публикацию продукта"),
+            ("can_delete_any_product", "Может удалять любой продукт"),
+        ]
     def __str__(self):
         return self.name
